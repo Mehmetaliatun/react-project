2b8d9416e89cb70f8ce7ed3ab20055bb7a7b6464
Atun <mali1996ua@gmail.com>
2022-10-08 16:10:05 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-function App() {
-  return <h1>hello</h1>;
-}
+import "./App.scss";
+import Home from "./component/Home";
+import Navbar from "./component/Navbar";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
+};
 
 export default App;
